0d8a15d0ab77d7b237b145063360e7fb51cc27c7
Zokirjon <42824341+mrZokir@users.noreply.github.com>
2021-11-26 23:02:20 +0500

    -from UZ to -> RU

--- app/src/main/java/profitdevs/group/anticor/util/utils/Constants.kt
+++ app/src/main/java/profitdevs/group/anticor/util/utils/Constants.kt
@@ -3,7 +3,7 @@ package profitdevs.group.anticor.util.utils
 class Constants{
     companion object{
        // const val BASE_URL = "https://eanticor.uz/uz/api/ref/"
-        const val BASE_URL = "https://eanticor.uz/uz/api/"
+        const val BASE_URL = "https://eanticor.uz/ru/api/"
         const val EXTRA_DATA = "extra_data"
         const val EXTRA_DATA_2 = "extra_data_2"
         const val EXTRA_DATA_START_FRAGMENT = "extra_data_start_fragment"
